078ed0fb65f53a35623205599d70b7c26a670d4a
Yash Raja <ryvijaykumar@tekion.com>
2023-01-23 14:40:42 +0530

    new branch change

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world from the cloned the repo and dont ask why, you dont have freedom");
+        System.out.println("Hello world from the cloned the repo and dont ask why, you dont have freedom,lets pretend for a brife sencond that you have");
         Bottleofbear objectname = new Bottleofbear();
         objectname.print_song(99);
     }
