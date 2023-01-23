c57132b3d2810a7fe43af91b3101af5402e3c7ad
Yash Raja <ryvijaykumar@tekion.com>
2023-01-23 13:08:57 +0530

    made some useless changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello world from the cloned the repo and dont ask why, you dont have freedom");
         Bottleofbear objectname = new Bottleofbear();
         objectname.print_song(99);
     }
